929969136713d7fb8d0697c84fdb7bd7325181d0
Anik Siddiky <codesbynik@gmail.com>
2025-05-26 19:45:01 +0600

    first commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from 'react-router';
+import RootLayout from '../Layouts/RootLayout';
+import Home from '../Pages/Home/Home';
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        Component: RootLayout,
+        children: [
+            {
+                index: true,
+                Component: Home,
+            }
+        ]
+    }
+])
+
+export default Router;
